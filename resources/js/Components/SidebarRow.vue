8f2e3c4d1e19e35c9e8d956e0664364196ff8cff
Jakub Furtak <kuba.furtak.pl@gmail.com>
2023-08-29 16:30:38 +0200

    fixed links in the sidebar

--- resources/js/Components/SidebarRow.vue
+++ resources/js/Components/SidebarRow.vue
@@ -24,13 +24,12 @@
 </script>
 
 <template>
-
-    <div class="flex items-center h-14 w-full duration-200 hover:bg-gray-500 cursor-pointer">
-        <img class="h-full mr-5 ml-2 justify-self-start" :src="url" alt="small icon">
-        <div v-if="!showText" class="flex-grow text-2xl ">
-            <p>{{text}}</p>
+        <div class="flex items-center h-14 w-full duration-200 hover:bg-gray-500 cursor-pointer">
+            <img class="h-full mr-5 ml-2 justify-self-start" :src="url" alt="small icon">
+            <div v-if="!showText" class="flex-grow text-2xl ">
+                <p>{{text}}</p>
+            </div>
         </div>
-    </div>
 </template>
 
 <style scoped>
--- resources/js/Layouts/SidebarLayout.vue
+++ resources/js/Layouts/SidebarLayout.vue
@@ -1,12 +1,13 @@
 <script setup>
 
 import {ref} from "vue";
-import { Link } from '@inertiajs/vue3';
+import {Link} from '@inertiajs/vue3';
 import NavLink from "@/Components/NavLink.vue";
 import SidebarRow from "@/Components/SidebarRow.vue";
 
 const isCollapsed = ref(false)
 const isToolsExpanded = ref(false)
+
 function onClick(e) {
     isCollapsed.value = !isCollapsed.value
 }
@@ -34,20 +35,29 @@ function mouseOverToolsLeave() {
 }
 </script>
 
-<template >
+<template>
     <div class="flex ">
         <div :class="{'w-48 transition-w duration-500': !isCollapsed, 'w-20 transition-w duration-500': isCollapsed}"
-             class="h-screen bg-[#2D2D30] text-white flex flex-col fixed top-0 left-0 " >
-            <div class="flex mr-2 flex-none cursor-pointer" :class="{'justify-center' : isCollapsed, 'justify-end ' : !isCollapsed}">
-<!--                burger icon-->
+             class="h-screen bg-[#2D2D30] text-white flex flex-col fixed top-0 left-0 ">
+            <div class="flex mr-2 flex-none cursor-pointer"
+                 :class="{'justify-center' : isCollapsed, 'justify-end ' : !isCollapsed}">
+                <!--                burger icon-->
                 <img src="../../images/menu_FILL0_wght400_GRAD0_opsz24.png" v-on:click="onClick" class="w-14">
             </div>
 
             <div id="tools" class="flex flex-col items-center grow">
-<!--                tools links section-->
-                <SidebarRow icon="home_FILL0_wght400_GRAD0_opsz24.png" text="home" :show-text="isCollapsed"/>
-                <SidebarRow icon="construction_FILL0_wght400_GRAD0_opsz24.png"  text="tools" :show-text="isCollapsed" @mouseenter="expandTools" @mouseleave="collapseTools"/>
-                <SidebarRow icon="folder_open_FILL0_wght400_GRAD0_opsz24.png" text="files" :show-text="isCollapsed"/>
+                <!--                tools links section-->
+                <Link href="/" class="flex items-center w-full">
+                    <SidebarRow icon="home_FILL0_wght400_GRAD0_opsz24.png" text="home" :show-text="isCollapsed"/>
+                </Link>
+                <!--                    </Link>-->
+
+                <SidebarRow icon="construction_FILL0_wght400_GRAD0_opsz24.png" text="tools" :show-text="isCollapsed"
+                            @mouseenter="expandTools" @mouseleave="collapseTools"/>
+                <Link href="#" class="flex items-center w-full">
+                    <SidebarRow icon="folder_open_FILL0_wght400_GRAD0_opsz24.png" text="files" :show-text="isCollapsed"/>
+                </Link>
+
                 <SidebarRow icon="help_FILL0_wght400_GRAD0_opsz24.png" text="help" :show-text="isCollapsed"/>
                 <SidebarRow icon="language_FILL0_wght400_GRAD0_opsz24.png" text="lang" :show-text="isCollapsed"/>
 
@@ -55,39 +65,56 @@ function mouseOverToolsLeave() {
 
 
             <div id="bottom" class="flex-none">
-<!--                user links section-->
-                <SidebarRow icon="contrast_FILL0_wght400_GRAD0_opsz24.png" text="contrast"  :show-text="isCollapsed"/>
+                <!--                user links section-->
+                <SidebarRow icon="contrast_FILL0_wght400_GRAD0_opsz24.png" text="contrast" :show-text="isCollapsed"/>
                 <SidebarRow icon="account_circle_FILL0_wght400_GRAD0_opsz24.png" text="user" :show-text="isCollapsed"/>
             </div>
 
         </div>
 
-        <div v-if="isToolsExpanded" class="w-48 bg-[#2D2D30] text-white absolute left-48 top-20" @mouseenter="mouseOverToolsEnter" @mouseleave="mouseOverToolsLeave">
-<!--            expanded tools-->
+        <div v-if="isToolsExpanded" class="w-48 bg-[#2D2D30] text-white absolute left-48 top-20 text-xl"
+             @mouseenter="mouseOverToolsEnter" @mouseleave="mouseOverToolsLeave">
+            <!--            expanded tools-->
             <ul>
-                <li class="hover:bg-gray-500 cursor-pointer">
-                    <Link href="/cutter" >Cutter</Link>
-                </li>
+                <Link href="/cutter">
+                    <li class="hover:bg-gray-500 cursor-pointer">
+                        Cutter
+                    </li>
+                </Link>
 
-                <li class="hover:bg-gray-500 cursor-pointer">
-                    <Link href="/metachange">Change metadata</Link>
-                </li>
 
-                <li class="hover:bg-gray-500 cursor-pointer">
-                    <Link href="/speedup">SpeedUp</Link>
-                </li>
+                <Link href="/metachange">
+                    <li class="hover:bg-gray-500 cursor-pointer">
+                        Change metadata
+                    </li>
+                </Link>
 
-                <li class="hover:bg-gray-500 cursor-pointer">
-                    <Link href="/merge">Merge</Link>
-                </li>
 
-                <li class="hover:bg-gray-500 cursor-pointer">
-                    <Link href="/recorder">Recorder</Link>
-                </li>
+                <Link href="/speedup">
+                    <li class="hover:bg-gray-500 cursor-pointer">
+                        SpeedUp
+                    </li>
+                </Link>
 
-                <li class="hover:bg-gray-500 cursor-pointer">
-                    <Link href="/layermixer">LayerMix</Link>
-                </li>
+
+                <Link href="/merge">
+                    <li class="hover:bg-gray-500 cursor-pointer">
+                        Merge
+                    </li>
+                </Link>
+
+                <Link href="/recorder">
+                    <li class="hover:bg-gray-500 cursor-pointer">
+                        Recorder
+                    </li>
+                </Link>
+
+
+                <Link href="/layermixer">
+                    <li class="hover:bg-gray-500 cursor-pointer">
+                        LayerMix
+                    </li>
+                </Link>
 
             </ul>
         </div>
