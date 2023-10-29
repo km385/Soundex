80cee26dcbe8caadacd593f085aadf5f26bf1324
Jakub Furtak <kuba.furtak.pl@gmail.com>
2023-10-29 12:27:18 +0100

    added visual cues for the user when interacting with UploadFile.vue and rounded the corners of the component further

--- resources/js/Pages/Tools/Partials/UploadFile.vue
+++ resources/js/Pages/Tools/Partials/UploadFile.vue
@@ -38,8 +38,17 @@ function onDrop(event) {
 
 function onDragOver(event) {
     console.log('handle drag over')
+
+    event.preventDefault()
+    document.getElementById('dragZone').classList.add('brightness-125')
+
+}
+function onDragLeave(event) {
+    console.log('leave')
+    document.getElementById('dragZone').classList.remove('brightness-125')
     event.preventDefault()
 }
+
 
 function onClick(event) {
     fileInput.value.click()
@@ -85,16 +94,17 @@ watch(uploadedFile, (value) => {
         </label>
         <input ref="fileInput" type="file" class="hidden" name="fileInput" id="fileInput" @change="handleFileChange">
     </div>
-    <div class="p-6 bg-gray-800 rounded-lg shadow-lg" v-if="!isButton">
+    <div id="dragZone" class="p-6 bg-gray-800 rounded-3xl shadow-lg cursor-pointer hover:brightness-125" v-if="!isButton"
+         @click="onClick" @dragover="onDragOver" @dragleave.self="onDragLeave" @drop="onDrop">
 
         <div
-            class="flex items-center justify-center w-[300px] h-[300px] border-[5px] border-dashed border-blue-500 "
-            @click="onClick" @dragover="onDragOver" @drop="onDrop">
+            class="flex items-center justify-center w-[300px] h-[300px] border-[5px] border-dashed border-blue-500 rounded-3xl ">
             <div class="flex flex-col items-center">
-<!--                <img class="w-1/2 h-1/2 mb-3" src="../../../../images/file_icon.webp" alt="file image">-->
-                <svg xmlns="http://www.w3.org/2000/svg" height="200" viewBox="0 -960 960 960" preserveAspectRatio="none">
+                <!--                <img class="w-1/2 h-1/2 mb-3" src="../../../../images/file_icon.webp" alt="file image">-->
+                <svg xmlns="http://www.w3.org/2000/svg" height="200" viewBox="0 -960 960 960"
+                     preserveAspectRatio="none">
                     <path fill="#000000"
-                        d="M240-160q-33 0-56.5-23.5T160-240v-80q0-17 11.5-28.5T200-360q17 0 28.5 11.5T240-320v80h480v-80q0-17 11.5-28.5T760-360q17 0 28.5 11.5T800-320v80q0 33-23.5 56.5T720-160H240Zm200-486-75 75q-12 12-28.5 11.5T308-572q-11-12-11.5-28t11.5-28l144-144q6-6 13-8.5t15-2.5q8 0 15 2.5t13 8.5l144 144q12 12 11.5 28T652-572q-12 12-28.5 12.5T595-571l-75-75v286q0 17-11.5 28.5T480-320q-17 0-28.5-11.5T440-360v-286Z"/>
+                          d="M240-160q-33 0-56.5-23.5T160-240v-80q0-17 11.5-28.5T200-360q17 0 28.5 11.5T240-320v80h480v-80q0-17 11.5-28.5T760-360q17 0 28.5 11.5T800-320v80q0 33-23.5 56.5T720-160H240Zm200-486-75 75q-12 12-28.5 11.5T308-572q-11-12-11.5-28t11.5-28l144-144q6-6 13-8.5t15-2.5q8 0 15 2.5t13 8.5l144 144q12 12 11.5 28T652-572q-12 12-28.5 12.5T595-571l-75-75v286q0 17-11.5 28.5T480-320q-17 0-28.5-11.5T440-360v-286Z"/>
                 </svg>
                 <p class="text-blue-700 font-bold">Upload new file</p>
             </div>
