616544be671ca45c86ff5a254721b4a149c0a224
Jakub Furtak <kuba.furtak.pl@gmail.com>
2023-12-28 15:47:11 +0100

    added a dash icon when the answer is shown

--- resources/js/Pages/Help.vue
+++ resources/js/Pages/Help.vue
@@ -32,22 +32,22 @@ function toggleQuestion(index) {
     <div class="my-10 mx-10 text-white">
         <div class="grid grid-cols-1 xl:grid-cols-2 gap-8">
             <div v-for="(question, index) in questions" class="max-w-lg sm:w-[512px]">
-
-                <div @click="toggleQuestion(index)" class="bg-gray-400 flex items-center px-4 cursor-pointer">
-                    <div class="mr-2 grow">
-                        <p>{{question.question}}</p>
+                    <div @click="toggleQuestion(index)" class="bg-gray-400 flex items-center px-4 cursor-pointer ">
+                        <div class="mr-2 grow">
+                            <p>{{question.question}}</p>
+                        </div>
+                        <div class="w-10 py-1 sm:h-12">
+                            <SvgComp v-if="openQuestions[index]" name="dash-lg" />
+                            <SvgComp v-else name="plus-lg" />
+                        </div>
                     </div>
-                    <div class="w-10 py-1 ">
-                        <SvgComp name="plus-lg" class="" />
-                    </div>
+                    <Transition name="slide-fade">
+                        <div v-if="openQuestions[index]" class="text-justify px-4 mt-2">
+                            {{question.answer}}
+                        </div>
+                    </Transition>
                 </div>
-                <Transition name="slide-fade">
-                    <div v-if="openQuestions[index]" class="text-justify px-3 mt-2">
-                        {{question.answer}}
-                    </div>
-                </Transition>
 
-            </div>
         </div>
     </div>
 </div>
