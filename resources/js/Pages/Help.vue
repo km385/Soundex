b36376060d295b26f474bc120f1bdfa748225ac4
Jakub Furtak <kuba.furtak.pl@gmail.com>
2023-12-30 18:08:22 +0100

    added localization to help page

--- resources/js/Pages/Help.vue
+++ resources/js/Pages/Help.vue
@@ -1,40 +1,34 @@
 <script setup>
 
 import SvgComp from "@/Components/SvgComp.vue";
-import {ref} from "vue";
-
-const questions = [
-    { question: 'Best tool for cutting and trimming audio files online?', answer: 'The Cutter tool is your go-to option for precise audio editing. Easily remove unwanted sections and customize your favorite tracks with precision and ease.' },
-    { question: 'Best tool for cutting and trimming audio files online?', answer: 'The Cutter tool is your go-to option for precise audio editing. Easily remove unwanted sections and customize your favorite tracks with precision and ease.' },
-    { question: 'Best tool for cutting and trimming audio files online?', answer: 'The Cutter tool is your go-to option for precise audio editing. Easily remove unwanted sections and customize your favorite tracks with precision and ease.' },
-    { question: 'Best tool for cutting and trimming audio files online?', answer: 'The Cutter tool is your go-to option for precise audio editing. Easily remove unwanted sections and customize your favorite tracks with precision and ease.' },
-    { question: 'Best tool for cutting and trimming audio files online?', answer: 'The Cutter tool is your go-to option for precise audio editing. Easily remove unwanted sections and customize your favorite tracks with precision and ease.' },
-    { question: 'Best tool for cutting and trimming audio files online?', answer: 'The Cutter tool is your go-to option for precise audio editing. Easily remove unwanted sections and customize your favorite tracks with precision and ease.' },
-    { question: 'Best tool for cutting and trimming audio files online?', answer: 'The Cutter tool is your go-to option for precise audio editing. Easily remove unwanted sections and customize your favorite tracks with precision and ease.' },
-    { question: 'Best tool for cutting and trimming audio files online?', answer: 'The Cutter tool is your go-to option for precise audio editing. Easily remove unwanted sections and customize your favorite tracks with precision and ease.' },
-    { question: 'Best tool for cutting and trimming audio files online?', answer: 'The Cutter tool is your go-to option for precise audio editing. Easily remove unwanted sections and customize your favorite tracks with precision and ease.' },
-    { question: 'Best tool for cutting and trimming audio files online?', answer: 'The Cutter tool is your go-to option for precise audio editing. Easily remove unwanted sections and customize your favorite tracks with precision and ease.' },
-
-];
+import {inject, ref} from "vue";
 
 const openQuestions = ref({});
-
 function toggleQuestion(index) {
     openQuestions.value[index] = !openQuestions.value[index];
 }
+
+const highContrast = inject('highContrast')
 </script>
 
 <template>
-<div class="flex flex-col items-center ">
-    <div class="mt-20 lg:mt-10">
-        <p class="text-5xl text-white">FAQ</p>
+<div
+
+    class="flex flex-col items-center text-white ">
+    <div
+        :class="{'high-contrast-input':highContrast}"
+        class="mt-20 lg:mt-10">
+        <p class="text-5xl">FAQ</p>
     </div>
     <div class="my-10 mx-10 text-white">
+
         <div class="grid grid-cols-1 xl:grid-cols-2 gap-8">
-            <div v-for="(question, index) in questions" class="max-w-lg sm:w-[512px]">
-                    <div @click="toggleQuestion(index)" class="bg-gray-400 flex items-center px-4 cursor-pointer ">
+            <div v-for="(content, index) in $tm('help')" class="max-w-lg sm:w-[512px]">
+                    <div @click="toggleQuestion(index)"
+                         :class="{'high-contrast-input':highContrast}"
+                         class="bg-gray-400 flex items-center px-4 cursor-pointer ">
                         <div class="mr-2 grow">
-                            <p>{{question.question}}</p>
+                            <p>{{$rt(content.question)}}</p>
                         </div>
                         <div class="w-10 py-1 sm:h-12">
                             <SvgComp v-if="openQuestions[index]" name="dash-lg" />
@@ -42,8 +36,10 @@ function toggleQuestion(index) {
                         </div>
                     </div>
                     <Transition name="slide-fade">
-                        <div v-if="openQuestions[index]" class="text-justify px-4 mt-2">
-                            {{question.answer}}
+                        <div v-if="openQuestions[index]"
+                             :class="{'high-contrast-input':highContrast}"
+                             class="text-justify px-4 mt-2">
+                            {{$rt(content.answer)}}
                         </div>
                     </Transition>
                 </div>
@@ -64,5 +60,8 @@ function toggleQuestion(index) {
     transform: translateY(-20px);
 }
 
+.high-contrast-input {
+    @apply text-xl border border-[#FFFF00FF] bg-black text-[#FFFF00FF]
+}
 
 </style>
