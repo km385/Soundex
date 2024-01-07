d68c704017289014b5707d1147750f53db6b28af
Mateusz HP <mateuszczernic.mc@gmail.com>
2024-01-07 18:10:04 +0100

    added formating to bpm table with contrast

--- resources/js/Pages/Tools/Partials/ResultOptionsScreen.vue
+++ resources/js/Pages/Tools/Partials/ResultOptionsScreen.vue
@@ -49,7 +49,7 @@ function toggleTableVisibility() {
 </script>
 
 <template>
-    <div class="text-white flex flex-col flex-grow justify-center items-center">
+    <div class="text-white flex flex-col flex-grow justify-center items-center mt-20 lg:mt-10">
 
         <!-- File Download Section -->
         <div :class="{ 'high-contrast-input': highContrast }" class="p-6 bg-gray-800 rounded-lg shadow-lg w-full">
@@ -61,31 +61,64 @@ function toggleTableVisibility() {
                 class="bg-blue-400 text-white rounded py-2 px-4 mt-10 hover:bg-blue-500 ">
                 {{ $t("resultOptionsScreen.hearAudio") }}
             </button>
-            <!-- TODO: formatowanie tabelki, przekazywanie value do metronomu-->
+            <!-- TODO:  przekazywanie value do metronomu-->
             <div v-if="bpmArray !== null">
 
-                <p v-html="$t('resultOptionsScreen.bpmFound', [bpmArray[0].BPM])"></p>
-                <br>
-                <p>{{ $t('resultOptionsScreen.bpmDescription') }}</p>
+                <p v-html="$t('resultOptionsScreen.bpmFound', [bpmArray[0].BPM])" class="mt-3"></p>
+                <p class="mt-3">{{ $t('resultOptionsScreen.bpmDescription') }}</p>
                 <button :class="{ 'high-contrast-button': highContrast }"
                     class="bg-blue-400 text-white rounded py-2 px-4 mt-4 hover:bg-blue-500" @click="toggleTableVisibility">
                     {{ $t('resultOptionsScreen.tableShowButton') }}
                 </button>
                 <div v-if="showTable">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>{{ $t('resultOptionsScreen.tableHeaderBpm') }}</th>
-                                <th>{{ $t('resultOptionsScreen.tableHeaderCount') }}</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr v-for="(item, index) in bpmArray.slice(1)" :key="index">
-                                <td>{{ item.BPM }}</td>
-                                <td>{{ item.Count }}</td>
-                            </tr>
-                        </tbody>
-                    </table>
+                    <div :class="{
+                        'border-[#FFFF00FF]': highContrast,
+                        'border-gray-300': !highContrast
+                    }" class="my-4 rounded-lg border overflow-hidden">
+                        <div class="flex flex-col">
+                            <div class="-m-1.5 overflow-x-auto">
+                                <div class="p-1.5 min-w-full inline-block align-middle">
+                                    <table :class="{
+                                        'divide-[#FFFF00FF]': highContrast,
+                                        'divide-gray-300': !highContrast
+                                    }" class="min-w-full divide-y table-auto text-center">
+                                        <thead :class="{ 'bg-gray-500': !highContrast }" class="rounded-lg">
+                                            <tr :class="{
+                                                'text-[#FFFF00FF] divide-[#FFFF00FF] text-xl': highContrast,
+                                                'text-gray-100 text-lg': !highContrast
+                                            }" class="divide-x">
+                                                <th scope="col" class="px-6 py-3  font-extrabold ">
+                                                    {{ $t('resultOptionsScreen.tableHeaderBpm') }}
+                                                </th>
+                                                <th scope="col" class="px-6 py-3  font-extrabold ">
+                                                    {{ $t('resultOptionsScreen.tableHeaderCount') }}
+                                                </th>
+                                            </tr>
+                                        </thead>
+                                        <tbody :class="{
+                                            'divide-[#FFFF00FF]  ': highContrast,
+                                            'divide-gray-200 bg-[#343541]': !highContrast
+                                        }" class="divide-y ">
+                                            <tr :class="{
+                                                'divide-[#FFFF00FF]': highContrast
+                                            }"
+                                                class="transition duration-300 ease-in-out hover:bg-gray-600  divide-x"
+                                                v-for="(item, index) in bpmArray.slice(1)" :key="index">
+                                                <td :class="{ 'text-gray-200': !highContrast }"
+                                                    class="px-6 py-4 whitespace-nowrap  font-medium ">
+                                                    {{ item.BPM + " BPM" }}
+                                                </td>
+                                                <td :class="{ 'text-gray-200': !highContrast }"
+                                                    class="px-6 py-4 whitespace-nowrap  font-medium ">
+                                                    {{ item.Count }}
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <p v-else>
